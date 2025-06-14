5ec7fb57df4d4563c49ead78dc7c4021e4354b37
Pedro Lucena <44994733+pedrolucena05@users.noreply.github.com>
2025-06-14 02:10:11 -0300

    Add files via upload

---
+++ virtual/dStruct.h
@@ -0,0 +1,13 @@
+#ifndef DSTRUCT_H
+#define DSTRUCT_H
+
+typedef struct carData {
+    unsigned char carVelocity;   
+    short enRotation;   
+    unsigned char enTemp;   
+    unsigned char exitFlag;
+    short adasQTD;
+    short atQTD;      
+} carData;
+
+#endif 
